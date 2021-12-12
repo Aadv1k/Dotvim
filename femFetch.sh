0c83a39b5b3ee78c9f0845e771d666cea51425dd
Aadvik <aadv1k@outlook.com>
2021-12-12 15:39:04 +0530

    improved the bash-scripts

--- femFetch.sh
+++ femFetch.sh
@@ -1,9 +1,23 @@
+
 if [ $# -eq 2  ] 
 then 
 	FOL=$2
-elif [ $# -eq 1 ]
+elif [ $# -eq 0 ]
 then 
-	echo Insufficient args, please provide fem link.
+	echo "
+   __           ___    _      _         _    
+  / _|___ _ __ | __|__| |_ __| |_    __| |_  
+ |  _/ -_) '  \| _/ -_)  _/ _| ' \ _(_-< ' \ 
+ |_| \___|_|_|_|_|\___|\__\__|_||_(_)__/_||_|
+	"
+ echo "
+USAGE: 
+ ./femFetch.sh <FEM-LINK> OUTPUT
+ ./femFetch.sh blogr-landing-page blogr
+ "
+
+
+	exit 1
 else 
 	FOL=$1
 fi
@@ -12,3 +26,5 @@ curl -O https://codeload.github.com/frontendmentorio/$1/zip/refs/heads/main
 unzip main -d $FOL
 mv $FOL/${1}-main/* $FOL/
 rm -rf $FOL/${1}-main main
+
+
